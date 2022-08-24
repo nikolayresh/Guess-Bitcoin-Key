745b18f367c6390e908b9490fe81f695a01beeac
Nikolay Reshetynskyi <777nickresh@gmail.com>
2022-08-24 01:17:10 -0400

    fixes

--- Source/Extensions.cs
+++ Source/Extensions.cs
@@ -1,12 +1,11 @@
 ﻿namespace GuessBitcoinKey
 {
-	internal static class Extensionscs
+	internal static class Extensions
 	{
 		internal static void Increment(this byte[] data)
 		{
 			int i = data.Length - 1;
-
-			while (i >= 0 && ++data[i] == byte.MinValue)
+			while (i >= 0 && ++data[i] == 0)
 			{
 				i--;
 			}
